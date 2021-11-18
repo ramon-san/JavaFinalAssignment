8b05ea3185bbb077d6c4b0715ef25c1432b74a39
Ramon-san <ramon.orraca@correo.uia.mx>
2021-11-18 17:12:21 -0600

    We add a few main files as an example.

---
+++ src/com/oop/fa/view/HeaderPanel.java
@@ -0,0 +1,29 @@
+package com.oop.fa.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HeaderPanel extends JPanel {
+    int imageWidth = 300;
+    int imageHeight = 130;
+    JLabel logo = new JLabel();
+    JLabel title = new JLabel("Proyecto final OOP 2021", SwingConstants.CENTER);
+    /* We make the image the same size as the label. */
+    ImageIcon originalFile = new ImageIcon("Logo_Ibero.jpg");
+    Image image = originalFile.getImage();
+    Image scaledImage = image.getScaledInstance(imageWidth, imageHeight, Image.SCALE_SMOOTH);
+    ImageIcon scaledLogo = new ImageIcon(scaledImage);
+
+    public HeaderPanel(){
+        setLayout(null);
+        title.setBounds(100, 100, 500,50);
+        title.setFont(new Font("Arial", Font.BOLD, 30));
+        title.setOpaque(true);
+        title.setBackground(Color.LIGHT_GRAY);
+        add(title);
+        logo.setBounds(700, 20, imageWidth,imageHeight);
+        logo.setIcon(scaledLogo);
+        add(logo);
+
+    }
+}
