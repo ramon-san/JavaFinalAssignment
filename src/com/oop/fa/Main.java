7b978f58a8ea87373717b0c38f9fa2d6dc703935
Ramon-san <ramon.orraca@correo.uia.mx>
2021-11-18 20:08:06 -0600

    Changed the position of our diagram files

--- src/com/oop/fa/Main.java
+++ src/com/oop/fa/Main.java
@@ -5,7 +5,6 @@ import com.oop.fa.view.MainView;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Program initialized.");
         MainView frame = new MainView();
         frame.setVisible(true);
