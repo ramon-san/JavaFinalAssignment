075687dcfd736702af35e3bdff5fcc7688cd0d64
Ramon-san <ramon.orraca@correo.uia.mx>
2021-11-21 23:18:11 -0600

    Fixed DB connection file and added the CSV we're using to create the DB.

--- src/com/oop/fa/Main.java
+++ src/com/oop/fa/Main.java
@@ -1,10 +1,14 @@
 package com.oop.fa;
 
+import com.oop.fa.controller.ConnectDB;
 import com.oop.fa.view.MainView;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        ConnectDB con = new ConnectDB();
+
         System.out.println("Program initialized.");
         MainView frame = new MainView();
         frame.setVisible(true);
